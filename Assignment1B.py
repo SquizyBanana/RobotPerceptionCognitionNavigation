253a8aa44d63e79804f633fd33d28cc0dec58c29
Cobalt <srozendom@gmail.com>
2025-12-10 15:15:11 +0100

    openCSVs

--- Assignment1B.py
+++ Assignment1B.py
@@ -1,17 +1,20 @@
 import numpy as np
 measurements = []
 
-for measurement in range(18):
-    if measurement < 9:
-        number = "0" + str(measurement+1)
-    else:
-        number = str(measurement+1)
-    file = open(f"Assignment 1B data\TBD","r")
 
+file1 = open("RobotPerceptionCognitionNavigation\Assingment 1B data\imu_417.csv","r")
+file2 = open("RobotPerceptionCognitionNavigation\Assingment 1B data\imu_423.csv","r")
+file3 = open("RobotPerceptionCognitionNavigation\Assingment 1B data\imu_425.csv","r")
+
+files = [file1,file2,file3]
+
+file_strings = []
+for file in files:
     raw_strings = file.readlines()
-
-    for i in range(13):
-        raw_strings.pop(0)
+    raw_strings.pop(0)
     strings = []
     for i in range(len(raw_strings)):
-        strings.append(raw_strings[i].split("\t"))
+        strings.append(raw_strings[i].split(","))
+    file_strings.append(strings)
+
+print(file_strings)
